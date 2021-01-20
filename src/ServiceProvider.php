f34ade38920ac913fafc4d64466441fd2dfd16fe
Andrey Helldar <helldar@ai-rus.com>
2021-01-20 17:04:04 +0300

    Optimized the location of folders without namespaces

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,22 +7,22 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '/config/settings.php' => $this->app->configPath('short_url.php'),
+            __DIR__ . '/../config/settings.php' => $this->app->configPath('short_url.php'),
         ], 'config');
 
         $this->loadMigrationsFrom(
-            __DIR__ . '/database/migrations'
+            __DIR__ . '/../database/migrations'
         );
 
         $this->loadRoutesFrom(
-            __DIR__ . '/routes/web.php'
+            __DIR__ . '/../routes/web.php'
         );
     }
 
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__ . '/config/settings.php',
+            __DIR__ . '/../config/settings.php',
             'short_url'
         );
     }
